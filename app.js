8133e7b2148e6a03b1f0f6a635e9e1439a50c14c
angela.brunst <angela.hairstylist@gmail.com>
2022-07-14 16:20:37 -0700

    Error handling added

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const { ApolloServer, gql } = require('apollo-server');
-const { connect, model, Schema } = require('mongoose');
+const { ApolloServer } = require('apollo-server');
+const { connect } = require('mongoose');
 const url = require('./src/db/config');
 const typeDefs = require('./src/typeDefs');
 const resolvers = require('./src/resolvers');
@@ -8,7 +8,21 @@ const port = process.env.PORT || 3000;
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    csrfPrevention: true,
+    cache: "bounded",
+    formatError: (err) => {
+        if (err.message.startsWith('Database Error: ')) {
+            return new Error('Internal server error');
+        }
+
+        return err;
+    },
+    formatError(err) {
+        if (err.originalError instanceof AuthenticationError) {
+            return new Error('Invalid Authentication');
+        }
+    },
 });
 
 connect(url)
