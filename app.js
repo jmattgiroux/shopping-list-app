ccdd4ef4e3a424d1e62905a59c0c0c175d6fc400
angela.brunst <angela.hairstylist@gmail.com>
2022-07-13 18:59:36 -0700

    Moved files under src file

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const { ApolloServer, gql } = require('apollo-server');
 const { connect, model, Schema } = require('mongoose');
-const url = require('./db/config');
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+const url = require('./src/db/config');
+const typeDefs = require('./src/typeDefs');
+const resolvers = require('./src/resolvers');
 
 const port = process.env.PORT || 3000;
 
