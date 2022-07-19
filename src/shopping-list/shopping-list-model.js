8ac34f25d215d8be2c6a5dd02b3593a5ad457063
MATTHEW CHRISTENSEN <matthewbchristensen@comcast.net>
2022-07-18 19:58:09 -0600

    returning id and createdAt correctly

--- src/shopping-list/shopping-list-model.js
+++ src/shopping-list/shopping-list-model.js
@@ -2,11 +2,11 @@ const { model, Schema } = require('mongoose');
 // const User = require('../users/users-model');
 
 const shoppingListSchema = new Schema({
-    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    user: String,
     ingredient: [{ type: Schema.Types.ObjectId, ref: 'Ingredient' }],
     createdAt: String
 });
 
-const ShoppingList = model('ShoppingList', shoppingListSchema);
+const ShoppingList = model('Shopping-List', shoppingListSchema);
 
 module.exports = ShoppingList;
