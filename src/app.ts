02fa73b6670b8a3b03304bd263324c01b03b53f2
angela.brunst <angela.hairstylist@gmail.com>
2022-07-18 20:38:36 -0700

    add error handling for graphQL and Database

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,16 @@ const port = process.env.PORT || 3000;
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    csrfPrevention: true,
+    cache: "bounded",
+    formatError: (err) => {
+        if (err.message.startsWith('Database Error: ')) {
+            return new Error('Internal server error');
+        }
+
+        return err;
+    }
 });
 
 
@@ -21,4 +30,8 @@ connect(url)
     .then((result) => {
         console.log("Server running at " + result.url);
     })
+    .catch((err) => {
+        console.log('Cannot connect to the database!', err);
+        process.exit();
+    });
 })();
