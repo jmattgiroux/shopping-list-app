977ba74a865c6f138e819e4491abf06665963e1d
jmattgiroux <jmattgiroux@gmail.com>
2022-07-20 21:37:05 -0600

    fixed minor typos

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,23 +1,23 @@
 (() => {
-const userResolvers = require('./database/users/users-resolvers');
-const ingredientResolvers = require('./database/ingredients/ingredients-resolvers');
-const shoppingListResolvers = require('./database/shopping-list/shopping-list-resolvers');
-const categoryResolvers = require('./database/categories/categories-resolvers');
+    const userResolvers = require("./database/users/users-resolvers");
+    const ingredientResolvers = require("./database/ingredients/ingredients-resolvers");
+    const shoppingListResolvers = require("./database/shopping-list/shopping-list-resolvers");
+    const categoryResolvers = require("./database/categories/categories-resolvers");
 
-const resolvers = {
-    Query: {
-        ...userResolvers.Query,
-        ...ingredientResolvers.Query,
-        ...shoppingListResolvers.Query
-        ...categoryResolvers.Query
-    },
-    Mutation: {
-        ...userResolvers.Mutation,
-        ...ingredientResolvers.Mutation,
-        ...shoppingListResolvers.Mutation
-        ...categoryResolvers.Mutation
-    },
-};
+    const resolvers = {
+        Query: {
+            ...userResolvers.Query,
+            ...ingredientResolvers.Query,
+            ...shoppingListResolvers.Query,
+            ...categoryResolvers.Query,
+        },
+        Mutation: {
+            ...userResolvers.Mutation,
+            ...ingredientResolvers.Mutation,
+            ...shoppingListResolvers.Mutation,
+            ...categoryResolvers.Mutation,
+        },
+    };
 
-module.exports = resolvers;
+    module.exports = resolvers;
 })();
